c3b8d829556deb6e9ff9fb72b3e20cd744e2c839
fahimnirjon <fahimnirjon21@gmail.com>
2024-10-19 17:51:48 +0600

    added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -42,6 +42,6 @@ export function constructMetadata({
       creator: '@joshtriedcoding',
     },
     icons,
-    metadataBase: new URL("https://case-cobra-sand.vercel.app/")
+    metadataBase: new URL("https://case-cobra-sand.vercel.app")
   }
 }
